d03f47cde647e12104e6b2e9d365333a3ac85595
phongnt71 <phongwary2004@gmail.com>
2025-06-15 12:03:30 +0700

    Update

--- QuanLyNhaHang/FormLogin.Designer.cs
+++ QuanLyNhaHang/FormLogin.Designer.cs
@@ -2,118 +2,129 @@
 {
     partial class FormLogin
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            txtUsername = new TextBox();
-            txtPassword = new TextBox();
-            label2 = new Label();
-            btnLogin = new Button();
-            btnExit = new Button();
-            SuspendLayout();
+            this.pictureBoxLogo = new PictureBox();
+            this.labelTitle = new Label();
+            this.labelUsername = new Label();
+            this.labelPassword = new Label();
+            this.txtUsername = new TextBox();
+            this.txtPassword = new TextBox();
+            this.btnLogin = new Button();
+            this.btnExit = new Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+
             // 
-            // label1
+            // pictureBoxLogo
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(220, 173);
-            label1.Name = "label1";
-            label1.Size = new Size(107, 20);
-            label1.TabIndex = 0;
-            label1.Text = "Tên đăng nhập";
+            this.pictureBoxLogo.Image = Image.FromFile("Images\\logo.png"); // <-- Đảm bảo file tồn tại ở đường dẫn này
+            this.pictureBoxLogo.SizeMode = PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo.Location = new Point(320, 20);
+            this.pictureBoxLogo.Size = new Size(150, 150);
+            this.pictureBoxLogo.TabStop = false;
+
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Text = "ĐĂNG NHẬP";
+            this.labelTitle.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            this.labelTitle.ForeColor = Color.SteelBlue;
+            this.labelTitle.TextAlign = ContentAlignment.MiddleCenter;
+            this.labelTitle.Location = new Point(0, 180);
+            this.labelTitle.Size = new Size(800, 40);
+
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.Text = "Tên đăng nhập:";
+            this.labelUsername.Font = new Font("Segoe UI", 10F);
+            this.labelUsername.Location = new Point(250, 240);
+            this.labelUsername.Size = new Size(120, 25);
+
             // 
             // txtUsername
             // 
-            txtUsername.Location = new Point(333, 166);
-            txtUsername.Name = "txtUsername";
-            txtUsername.Size = new Size(194, 27);
-            txtUsername.TabIndex = 1;
+            this.txtUsername.Location = new Point(380, 238);
+            this.txtUsername.Size = new Size(170, 27);
+
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.Text = "Mật khẩu:";
+            this.labelPassword.Font = new Font("Segoe UI", 10F);
+            this.labelPassword.Location = new Point(250, 280);
+            this.labelPassword.Size = new Size(120, 25);
+
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(333, 199);
-            txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(194, 27);
-            txtPassword.TabIndex = 3;
-            txtPassword.UseSystemPasswordChar = true;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(220, 206);
-            label2.Name = "label2";
-            label2.Size = new Size(70, 20);
-            label2.TabIndex = 2;
-            label2.Text = "Mật khẩu";
+            this.txtPassword.Location = new Point(380, 278);
+            this.txtPassword.Size = new Size(170, 27);
+            this.txtPassword.UseSystemPasswordChar = true;
+
             // 
             // btnLogin
             // 
-            btnLogin.Location = new Point(333, 232);
-            btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(94, 29);
-            btnLogin.TabIndex = 4;
-            btnLogin.Text = "Đăng nhập";
-            btnLogin.UseVisualStyleBackColor = true;
-            btnLogin.Click += BtnLogin_Click;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            this.btnLogin.BackColor = Color.SteelBlue;
+            this.btnLogin.ForeColor = Color.White;
+            this.btnLogin.FlatStyle = FlatStyle.Flat;
+            this.btnLogin.Location = new Point(380, 320);
+            this.btnLogin.Size = new Size(100, 35);
+            this.btnLogin.Click += BtnLogin_Click;
+
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(433, 232);
-            btnExit.Name = "btnExit";
-            btnExit.Size = new Size(94, 29);
-            btnExit.TabIndex = 5;
-            btnExit.Text = "Thoát";
-            btnExit.UseVisualStyleBackColor = true;
-            btnExit.Click += BtnExit_Click;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.Font = new Font("Segoe UI", 10F);
+            this.btnExit.Location = new Point(490, 320);
+            this.btnExit.Size = new Size(60, 35);
+            this.btnExit.Click += BtnExit_Click;
+
             // 
             // FormLogin
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(btnExit);
-            Controls.Add(btnLogin);
-            Controls.Add(txtPassword);
-            Controls.Add(label2);
-            Controls.Add(txtUsername);
-            Controls.Add(label1);
-            Name = "FormLogin";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "FormLogin";
-            Load += FormLogin_Load;
-            ResumeLayout(false);
-            PerformLayout();
+            this.AcceptButton = this.btnLogin;
+            this.ClientSize = new Size(800, 420);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnExit);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập hệ thống";
+            this.Load += FormLogin_Load;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
+        private PictureBox pictureBoxLogo;
+        private Label labelTitle;
+        private Label labelUsername;
+        private Label labelPassword;
         private TextBox txtUsername;
         private TextBox txtPassword;
-        private Label label2;
         private Button btnLogin;
         private Button btnExit;
     }
