965e5948e093f076013b3260d571b34fe357170c
phongnt71 <phongwary2004@gmail.com>
2025-06-15 12:25:35 +0700

    Update

--- QuanLyNhaHang/FormLogin.Designer.cs
+++ QuanLyNhaHang/FormLogin.Designer.cs
@@ -26,79 +26,64 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
             this.SuspendLayout();
 
-            // 
             // pictureBoxLogo
-            // 
-            this.pictureBoxLogo.Image = Image.FromFile("Images\\logo.png"); // <-- Đảm bảo file tồn tại ở đường dẫn này
+            string logoPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images\\LogoLogin.png");
+            if (File.Exists(logoPath))
+                this.pictureBoxLogo.Image = Image.FromFile(logoPath);
             this.pictureBoxLogo.SizeMode = PictureBoxSizeMode.Zoom;
             this.pictureBoxLogo.Location = new Point(320, 20);
             this.pictureBoxLogo.Size = new Size(150, 150);
             this.pictureBoxLogo.TabStop = false;
 
-            // 
             // labelTitle
-            // 
             this.labelTitle.Text = "ĐĂNG NHẬP";
             this.labelTitle.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
-            this.labelTitle.ForeColor = Color.SteelBlue;
+            this.labelTitle.ForeColor = Color.FromArgb(34, 112, 52); // Màu xanh lá từ logo
             this.labelTitle.TextAlign = ContentAlignment.MiddleCenter;
             this.labelTitle.Location = new Point(0, 180);
             this.labelTitle.Size = new Size(800, 40);
 
-            // 
             // labelUsername
-            // 
             this.labelUsername.Text = "Tên đăng nhập:";
             this.labelUsername.Font = new Font("Segoe UI", 10F);
             this.labelUsername.Location = new Point(250, 240);
             this.labelUsername.Size = new Size(120, 25);
 
-            // 
             // txtUsername
-            // 
             this.txtUsername.Location = new Point(380, 238);
             this.txtUsername.Size = new Size(170, 27);
+            this.txtUsername.KeyDown += new KeyEventHandler(this.TxtUsername_KeyDown);
 
-            // 
             // labelPassword
-            // 
             this.labelPassword.Text = "Mật khẩu:";
             this.labelPassword.Font = new Font("Segoe UI", 10F);
             this.labelPassword.Location = new Point(250, 280);
             this.labelPassword.Size = new Size(120, 25);
 
-            // 
             // txtPassword
-            // 
             this.txtPassword.Location = new Point(380, 278);
             this.txtPassword.Size = new Size(170, 27);
             this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.KeyDown += new KeyEventHandler(this.TxtPassword_KeyDown);
 
-            // 
             // btnLogin
-            // 
             this.btnLogin.Text = "Đăng nhập";
             this.btnLogin.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            this.btnLogin.BackColor = Color.SteelBlue;
+            this.btnLogin.BackColor = Color.FromArgb(34, 112, 52);
             this.btnLogin.ForeColor = Color.White;
             this.btnLogin.FlatStyle = FlatStyle.Flat;
             this.btnLogin.Location = new Point(380, 320);
             this.btnLogin.Size = new Size(100, 35);
             this.btnLogin.Click += BtnLogin_Click;
 
-            // 
             // btnExit
-            // 
             this.btnExit.Text = "Thoát";
             this.btnExit.Font = new Font("Segoe UI", 10F);
             this.btnExit.Location = new Point(490, 320);
             this.btnExit.Size = new Size(60, 35);
             this.btnExit.Click += BtnExit_Click;
 
-            // 
             // FormLogin
-            // 
-            this.AcceptButton = this.btnLogin;
             this.ClientSize = new Size(800, 420);
             this.Controls.Add(this.pictureBoxLogo);
             this.Controls.Add(this.labelTitle);
@@ -110,8 +95,9 @@
             this.Controls.Add(this.btnExit);
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.StartPosition = FormStartPosition.CenterScreen;
-            this.Text = "Đăng nhập hệ thống";
+            this.Text = "FormLogin";
             this.Load += FormLogin_Load;
+
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
